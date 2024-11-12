93dc14d765cc7790783483c3eaa4192613c4415d
baotam546 <donguyenbaotam546@gmail.com>
2024-11-12 10:36:22 +0700

    fix null avatar [TamDNB]

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { CiSettings } from "react-icons/ci";
-import { IoIosNotificationsOutline } from "react-icons/io";
-import Colors from '@/constants/color'
+import React from 'react';
+import { CiSettings } from 'react-icons/ci';
+import { IoIosNotificationsOutline } from 'react-icons/io';
+import Colors from '@/constants/color';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { useGetProfile } from '@/hooks/useGetProfile';
 import { Label } from './ui/Label';
@@ -12,67 +12,72 @@ import {
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/DropdownMenu"
+  DropdownMenuTrigger
+} from '@/components/ui/DropdownMenu';
 import { convertTitleToTitleCase } from '@/helpers/convertTitleToCaseTitle';
 import { Button } from './ui/button';
 import { LogOut, User } from 'lucide-react';
 import placeholder from '@/assets/images/avatar.png';
 import useLogout from '@/hooks/useLogout';
-type Props = {}
+type Props = {};
 
 const TopBar = (props: Props) => {
   const logout = useLogout();
   const user = useGetProfile();
-    const iconSize = 32;
-    const blue = Colors.blue[500];
+  const iconSize = 32;
+  const blue = Colors.blue[500];
   return (
-    <div className='w-full h-20 pl-6 flex bg-white'>
-    
-    <div className='w-full flex gap-2 justify-end items-center pr-8'>
-    <IoIosNotificationsOutline color={blue} size={iconSize}/>
+    <div className="w-full h-20 pl-6 flex bg-white">
+      <div className="w-full flex gap-2 justify-end items-center pr-8">
+        <IoIosNotificationsOutline color={blue} size={iconSize} />
 
-    <CiSettings color={blue} size={iconSize}/>
-    
-    <DropdownMenu>
-  <DropdownMenuTrigger>
-  <Button variant="ghost" className="relative h-10 w-10 rounded-full">
+        <CiSettings color={blue} size={iconSize} />
+
+        {user ? (
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Button variant="ghost" className="relative h-10 w-10 rounded-full">
+                <Avatar className="h-10 w-10">
+                  <AvatarImage src={user.avatar || placeholder} alt="Profile picture" />
+                  <AvatarFallback>{user.name}</AvatarFallback>
+                </Avatar>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-72" align="end" forceMount>
+              <DropdownMenuLabel>
+                <div className="flex flex-col space-y-1">
+                  <p className="text-lg font-medium leading-none">{user.name}</p>
+                  <p className="text-sm leading-none text-muted-foreground">
+                    {convertTitleToTitleCase(user.role)}
+                  </p>
+                </div>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  <User className="mr-2 h-4 w-4" />
+                  <div className="flex flex-col space-y-1">
+                    <p className="leading-none">Contact Information</p>
+                    <p className="text-sm text-muted-foreground">{user.email}</p>
+                    <p className="text-sm text-muted-foreground">{user.phone}</p>
+                  </div>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuItem onClick={logout} className="text-red-600">
+                <LogOut className="mr-2 h-4 w-4" />
+                <span>Log out</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        ) : (
           <Avatar className="h-10 w-10">
-            <AvatarImage src={user.avatar || placeholder} alt="Profile picture" />
+            <AvatarImage src={placeholder} alt="Profile picture" />
             <AvatarFallback>KT</AvatarFallback>
           </Avatar>
-        </Button>
-  </DropdownMenuTrigger>
-  <DropdownMenuContent className="w-72" align="end" forceMount>
-        <DropdownMenuLabel>
-        <div className="flex flex-col space-y-1">
-            <p className="text-lg font-medium leading-none">{user.name}</p>
-            <p className="text-sm leading-none text-muted-foreground">{convertTitleToTitleCase(user.role)}</p>
-          </div>
-        </DropdownMenuLabel>
-    <DropdownMenuSeparator />
-    <DropdownMenuGroup>
-          <DropdownMenuItem>
-            <User className="mr-2 h-4 w-4" />
-            <div className="flex flex-col space-y-1">
-              <p className="leading-none">Contact Information</p>
-              <p className="text-sm text-muted-foreground">{user.email}</p>
-              <p className="text-sm text-muted-foreground">{user.phone}</p>
-            </div>
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-        <DropdownMenuItem onClick={logout} className="text-red-600">
-          <LogOut className="mr-2 h-4 w-4" />
-          <span>Log out</span>
-        </DropdownMenuItem>
-  </DropdownMenuContent>
-</DropdownMenu>
-    
-    
+        )}
+      </div>
     </div>
-    
-    </div>
-  )
-}
+  );
+};
 
-export default TopBar
+export default TopBar;
--- src/hooks/useGetProfile.ts
+++ src/hooks/useGetProfile.ts
@@ -1,4 +1,14 @@
+import { toast } from "./use-toast";
+
 export const useGetProfile = ()=>{
     const user = localStorage.getItem('userData');
+    if (user === null) {
+        window.location.href = '/login';
+        toast({
+            title: 'Out of Session',
+            variant: 'destructive',
+            description: 'Please login again'
+        })
+    }
     return user ? JSON.parse(user) : null;
 }
