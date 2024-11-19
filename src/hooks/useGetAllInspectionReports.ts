9f75f8532104a1b41761f4f1e0d31230e32457d7
Nguyen Dinh Trung Kien <devlindinh2003@gmail.com>
2024-11-19 13:20:55 +0700

    chore: get all inspection report [KienNDT]

---
+++ src/hooks/useGetAllInspectionReports.ts
@@ -0,0 +1,38 @@
+import { getAllInspectionReports } from '@/api/services/inspectionReport';
+import { InspectionReportListResponse } from '@/types/InspectionReportListResponse';
+import { useQuery } from '@tanstack/react-query';
+import { ColumnFiltersState, PaginationState, SortingState } from '@tanstack/react-table';
+import { AxiosError } from 'axios';
+
+interface UseGetAllPurchaseOrderParams {
+  sorting: SortingState;
+  columnFilters: ColumnFiltersState;
+  pagination: PaginationState;
+}
+
+export const useGetAllInspectionReport = ({
+  sorting,
+  columnFilters,
+  pagination
+}: UseGetAllPurchaseOrderParams) => {
+  const {
+    data,
+    status,
+    isLoading: isPending,
+    isError,
+    isSuccess,
+    isFetching
+  } = useQuery<InspectionReportListResponse, AxiosError>({
+    queryKey: [
+      'purchaseOrdersList',
+      sorting,
+      columnFilters,
+      pagination.pageIndex,
+      pagination.pageSize
+    ],
+    queryFn: () => getAllInspectionReports({ sorting, columnFilters, pagination })
+  });
+  const purchaseOrderList = data?.data;
+  const pageMeta = data?.pageMeta;
+  return { data, status, isPending, isFetching, isError, isSuccess, pageMeta, purchaseOrderList };
+};
