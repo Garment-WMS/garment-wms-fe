8c145dbc70588d0472f69ea2f65be772a85d053a
Nguyen Dinh Trung Kien <devlindinh2003@gmail.com>
2024-11-21 14:07:02 +0700

    chore: production batch list [KienNDT]

--- src/pages/Production Batch/management/index.tsx
+++ src/pages/Production Batch/management/index.tsx
@@ -1,11 +1,21 @@
+import { Button } from '@/components/ui/button';
 import ProductionBatchIntroduction from './components/Introduction';
+import { Plus } from 'lucide-react';
+import ProductionBatchList from './components/ProductionBatchList';
 
 const ProductionBatchManagement = () => {
   return (
     <div className="h-auto w-full px-4 py-3 flex flex-col space-y-3">
       {/* Introduction */}
       <ProductionBatchIntroduction />
+      {/* Import Buttonm */}
+      <div className="flex justify-end items-center mb-6">
+        <Button>
+          <Plus className="mr-2 h-4 w-4" /> Add New Plan
+        </Button>
+      </div>
       {/* Production Batch List */}
+      <ProductionBatchList />
     </div>
   );
 };
