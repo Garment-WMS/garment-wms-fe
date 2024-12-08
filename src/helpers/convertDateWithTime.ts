550d0cdf5bbe19c740f1d135689857584ed8a625
Trung Kien Nguyễn Đình <devlindinh2003@gmail.com>
2024-12-08 11:12:16 +0700

    hotfix: po [KienNDT]

--- src/helpers/convertDateWithTime.ts
+++ src/helpers/convertDateWithTime.ts
@@ -3,13 +3,13 @@ export const convertDateWithTime = (isoString: string): string => {
     const date = new Date(isoString);
 
     // Format date and time
-    const formattedDate = date.toLocaleDateString('en-US', {
+    const formattedDate = date.toLocaleDateString('vi-VN', {
       year: 'numeric',
       month: '2-digit',
       day: '2-digit'
     });
 
-    const formattedTime = date.toLocaleTimeString('en-US', {
+    const formattedTime = date.toLocaleTimeString('vi-VN', {
       hour: '2-digit',
       minute: '2-digit',
       hour12: false
