fe2564951717ac8405ee27560b0ab03a5ef40662
Trung Kien Nguyễn Đình <devlindinh2003@gmail.com>
2024-12-15 10:37:41 +0700

    hotfix

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -52,43 +52,49 @@ const TopBar = (props: Props) => {
     <div className="w-full h-20 pl-6 flex bg-white">
       <div className="w-full flex gap-2 justify-end items-center pr-8">
         <ScannerPopup />
-        <NotificationListener/>
+        <NotificationListener />
         {user ? (
-          <DropdownMenu>
-            <DropdownMenuTrigger>
-              <Button variant="ghost" className="relative h-10 w-10 rounded-full">
-                <Avatar className="h-10 w-10">
-                  <AvatarImage src={user.avatar || placeholder} alt="Profile picture" />
-                  <AvatarFallback>{user.name}</AvatarFallback>
-                </Avatar>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-72" align="end" forceMount>
-              <DropdownMenuLabel>
-                <div className="flex flex-col space-y-1">
-                  <p className="text-lg font-medium leading-none">{user.name}</p>
-                  <p className="text-sm leading-none text-muted-foreground">
-                    {convertTitleToTitleCase(user.role)}
-                  </p>
-                </div>
-              </DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuGroup>
-                <DropdownMenuItem>
-                  <User className="mr-2 h-4 w-4" />
+          <div className="flex items-center gap-4">
+            <div className="flex flex-col items-end">
+              <p className="text-sm font-medium leading-none">{user.name}</p>
+              <p className="text-xs text-muted-foreground">{convertTitleToTitleCase(user.role)}</p>
+            </div>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Button variant="ghost" className="relative h-10 w-10 rounded-full">
+                  <Avatar className="h-10 w-10">
+                    <AvatarImage src={user.avatar || placeholder} alt="Profile picture" />
+                    <AvatarFallback>{user.name}</AvatarFallback>
+                  </Avatar>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-72" align="end" forceMount>
+                <DropdownMenuLabel>
                   <div className="flex flex-col space-y-1">
-                    <p className="leading-none">Contact Information</p>
-                    <p className="text-sm text-muted-foreground">{user.email}</p>
-                    <p className="text-sm text-muted-foreground">{user.phone}</p>
+                    <p className="text-lg font-medium leading-none">{user.name}</p>
+                    <p className="text-sm leading-none text-muted-foreground">
+                      {convertTitleToTitleCase(user.role)}
+                    </p>
                   </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <User className="mr-2 h-4 w-4" />
+                    <div className="flex flex-col space-y-1">
+                      <p className="leading-none">Contact Information</p>
+                      <p className="text-sm text-muted-foreground">{user.email}</p>
+                      <p className="text-sm text-muted-foreground">{user.phone}</p>
+                    </div>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuItem onClick={logout} className="text-red-600">
+                  <LogOut className="mr-2 h-4 w-4" />
+                  <span>Log out</span>
                 </DropdownMenuItem>
-              </DropdownMenuGroup>
-              <DropdownMenuItem onClick={logout} className="text-red-600">
-                <LogOut className="mr-2 h-4 w-4" />
-                <span>Log out</span>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         ) : (
           <Avatar className="h-10 w-10">
             <AvatarImage src={placeholder} alt="Profile picture" />
